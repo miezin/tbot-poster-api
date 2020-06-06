2c81cd7e7438f2dbc759607cd74865245c61935f
Grigory Miezin <grigory@leanconvert.com>
2020-06-07 01:29:53 +0300

    added user name to sessions

--- src/middlewares/session-saver.ts
+++ src/middlewares/session-saver.ts
@@ -2,18 +2,25 @@ import { Mongoose, HookNextFunction } from "mongoose";
 import { ContextMessageUpdate } from "telegraf-context";
 
 export const sessionSaver = (mongoose: Mongoose) => {
-    return async (ctx: ContextMessageUpdate, next: HookNextFunction) => {
-        const { db } = mongoose.connection;
-        const collection = db.collection('session');
-        if (!Object.keys(ctx.session).length) {
-            const session = await collection.findOne({ key: ctx.from.id });
-            if (session) {
-                ctx.session = session.data;
-            }
-        }
-
-        await next();
-        await collection.updateOne({ key: ctx.from.id }, { $set: { data: ctx.session } }, { upsert: true });
-
+  return async (ctx: ContextMessageUpdate, next: HookNextFunction) => {
+    const { db } = mongoose.connection;
+    const collection = db.collection('session');
+    if (!Object.keys(ctx.session).length) {
+      const session = await collection.findOne({ key: ctx.from.id });
+      if (session) {
+        ctx.session = session.data;
+      }
     }
+
+    await next();
+    await collection.updateOne(
+      {
+        key: ctx.from.id,
+        firstName: ctx.from.first_name,
+        username: ctx.from.username
+       },
+      { $set: { data: ctx.session } },
+      { upsert: true }
+      );
+  }
 }
