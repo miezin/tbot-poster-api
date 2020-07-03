77c0b719d893449c9d60d6b57622ccbb7c1dd361
Grigory Miezin <grigory@leanconvert.com>
2020-07-04 01:39:23 +0300

    added user activity middleware,  refactoring session middleware

--- src/middlewares/session-saver.ts
+++ src/middlewares/session-saver.ts
@@ -1,24 +1,27 @@
 import { Mongoose } from "mongoose";
 import { ContextMessageUpdate } from "telegraf-context";
+import User from "../models/User";
 
 export const sessionSaver = (mongoose: Mongoose) => {
   return async (ctx: ContextMessageUpdate, next: () => Promise<void>) => {
     const { db } = mongoose.connection;
-    const collection = db.collection('session');
+    const sessions = db.collection('sessions');
     if (!Object.keys(ctx.session).length) {
-      const session = await collection.findOne({ key: ctx.from.id });
+      const session = await sessions.findOne({ key: ctx.from.id });
+
       if (session) {
         ctx.session = session.data;
       }
     }
 
     await next();
-    await collection.updateOne(
+    await sessions.updateOne(
       {
         key: ctx.from.id
-       },
+      },
       {
-        $set: { data: ctx.session,
+        $set: {
+          data: ctx.session,
           firstName: ctx.from.first_name,
           username: ctx.from.username
         }
